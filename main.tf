b101e3e26bbda54d76eb3e778b0b89db699ec01c
Peter Kraft <petereliaskraft@gmail.com>
2024-06-04 12:34:38 -0700

    rename things

--- main.tf
+++ main.tf
@@ -23,36 +23,47 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "null_resource" "trigger_upload" {
+  triggers = {
+    zip_last_modified = filemd5("${path.module}/lambda/hello_world.zip")
+  }
+
+  provisioner "local-exec" {
+    command = "echo Re-uploading Lambda function due to changes"
+  }
+}
+
 resource "aws_lambda_function" "hello_world" {
   function_name = "HelloWorldFunction"
   handler       = "hello_world.handler"
   runtime       = "nodejs20.x"
   filename      = "lambda/hello_world.zip"
 
-  role = aws_iam_role.lambda_exec_role.arn
+  role       = aws_iam_role.lambda_exec_role.arn
+  depends_on = [null_resource.trigger_upload]
 }
 
-resource "aws_api_gateway_rest_api" "example_api" {
-  name = "ExampleAPI"
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name = "LambdaBenchmarkingAPI"
 }
 
-resource "aws_api_gateway_resource" "example_resource" {
-  rest_api_id = aws_api_gateway_rest_api.example_api.id
-  parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
-  path_part   = "example-path"
+resource "aws_api_gateway_resource" "lambda_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  path_part   = "hello-world"
 }
 
-resource "aws_api_gateway_method" "example_method" {
-  rest_api_id   = aws_api_gateway_rest_api.example_api.id
-  resource_id   = aws_api_gateway_resource.example_resource.id
+resource "aws_api_gateway_method" "lambda_gateway_method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_resource.lambda_gateway_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.example_api.id
-  resource_id = aws_api_gateway_resource.example_resource.id
-  http_method = aws_api_gateway_method.example_method.http_method
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.lambda_gateway_resource.id
+  http_method = aws_api_gateway_method.lambda_gateway_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -65,19 +76,19 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   function_name = aws_lambda_function.hello_world.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.example_api.execution_arn}/*/*/example-path"
+  source_arn = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*/hello-world"
 }
 
 
-resource "aws_api_gateway_deployment" "example_deployment" {
+resource "aws_api_gateway_deployment" "lambda_gateway_deployment" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
   ]
 
-  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   stage_name  = "test"
 }
 
 output "invoke_url" {
-  value = "${aws_api_gateway_deployment.example_deployment.invoke_url}/${aws_api_gateway_resource.example_resource.path_part}"
+  value = "${aws_api_gateway_deployment.lambda_gateway_deployment.invoke_url}/${aws_api_gateway_resource.lambda_gateway_resource.path_part}"
 }
