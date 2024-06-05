e76e18b9893b90f69d3c2e4350fa0d145130d39b
Peter Kraft <petereliaskraft@gmail.com>
2024-06-05 13:31:10 -0700

    real transaction

--- lambda/lambda_transaction/index.js
+++ lambda/lambda_transaction/index.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg');
 
 let client;
+const user = "dbos";
 
 const connectToDatabase = async (hostname, username, password) => {
     if (!client) {
@@ -20,14 +21,24 @@ exports.handler = async (event) => {
 
     try {
         await connectToDatabase(hostname, username, password);
-        const res = await client.query('SELECT 1');
-
+        await client.query("BEGIN");
+        const read_query = "SELECT COALESCE((SELECT greet_count FROM dbos_hello WHERE name = $1), 0) AS greet_count;";
+        const { rows } = await client.query(read_query, [user]);
+        const greet_count = rows[0].greet_count;
+        let write_query;
+        if (greet_count > 0) {
+          write_query = "UPDATE dbos_hello SET greet_count=$1 WHERE name=$2;";
+        } else {
+          write_query = "INSERT INTO dbos_hello (greet_count, name) VALUES ($1, $2);";
+        }
+        await client.query(write_query, [greet_count + 1, user]);
+        await client.query("COMMIT");
         const response = {
             statusCode: 200,
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(res.rows)
+            body: JSON.stringify(`Hello, ${user}! You have been greeted ${greet_count + 1} times.\n`)
         };
         return response;
     } catch (error) {
