7909bae776bfadb09c73439ea90046ecd2a4f207
Peter Kraft <petereliaskraft@gmail.com>
2024-06-04 17:27:13 -0700

    code

--- dbos-benchmark-app/src/operations.ts
+++ dbos-benchmark-app/src/operations.ts
@@ -1,4 +1,4 @@
-import { TransactionContext, Transaction, GetApi, ArgSource, ArgSources } from '@dbos-inc/dbos-sdk';
+import { TransactionContext, Transaction, GetApi, WorkflowContext, Workflow } from '@dbos-inc/dbos-sdk';
 import { Knex } from 'knex';
 
 // The schema of the database table used in this example.
@@ -9,13 +9,23 @@ export interface dbos_hello {
 
 export class Hello {
 
-  @GetApi('/greeting/:user') // Serve this function from HTTP GET requests to the /greeting endpoint with 'user' as a path parameter
-  @Transaction()  // Run this function as a database transaction
-  static async helloTransaction(ctxt: TransactionContext<Knex>, @ArgSource(ArgSources.URL) user: string) {
+  @Transaction()
+  static async helloTransaction(ctxt: TransactionContext<Knex>, user: string) {
     // Retrieve and increment the number of times this user has been greeted.
     const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
     const { rows } = await ctxt.client.raw(query, [user]) as { rows: dbos_hello[] };
     const greet_count = rows[0].greet_count;
     return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
+  }
+
+  @GetApi('/:num')
+  @Workflow()
+  static async helloWorkflow(ctxt: WorkflowContext, num: number) {
+    const start = performance.now();
+    for (let i = 0; i < num; i++) {
+      await ctxt.invoke(Hello).helloTransaction("dbos");
+    }
+    const end = performance.now();
+    return end - start;
   }
 }
