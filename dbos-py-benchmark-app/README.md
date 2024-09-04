0b6d94f748c956173d242bcb14d7a5b2eedf0b6a
Qian Li <qian.li@dbos.dev>
2024-09-03 18:49:22 -0700

    add python bench

---
+++ dbos-py-benchmark-app/README.md
@@ -0,0 +1,1 @@
+# dbos-py-benchmark-app
---
+++ dbos-py-benchmark-app/dbos_py_benchmark_app/main.py
@@ -0,0 +1,63 @@
+import time
+from typing import TypedDict
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from sqlalchemy.dialects.postgresql import insert
+
+from dbos import DBOS
+
+from .schema import dbos_hello
+
+app = FastAPI()
+DBOS(app)
+
+class Perf(TypedDict):
+    output: str
+    runtime: float
+
+@DBOS.transaction()
+def bench_transaction(name: str) -> str:
+    query = (
+        insert(dbos_hello)
+        .values(name="dbos", greet_count=1)
+        .on_conflict_do_update(
+            index_elements=["name"], set_={"greet_count": dbos_hello.c.greet_count + 1}
+        )
+        .returning(dbos_hello.c.greet_count)
+    )
+    greet_count = DBOS.sql_session.execute(query).scalar_one()
+    greeting = f"Greetings, {name}! You have been greeted {greet_count} times."
+    DBOS.logger.info(greeting)
+    return greeting
+
+@DBOS.workflow()
+def bench_workflow(num: int) -> str:
+    output = ""
+    for i in range(num):
+        output = bench_transaction(f"dbos-{i}")
+    return output
+
+# Single transaction workflow
+@app.get("/txn/{num}")
+def handler_transaction(num: int) -> Perf:
+    start = time.time()
+    output = bench_transaction(f"dbos-{num}")
+    duration = (time.time() - start) * 1000
+    return {"output": output, "runtime": duration}
+
+# Multiple transaction workflow
+@app.get("/wf/{num}")
+def handler_workflow(num: int) -> Perf:
+    start = time.time()
+    output = bench_workflow(int(num))
+    duration = (time.time() - start) * 1000
+    return {"output": output, "runtime": duration}
+
+# Bare handler
+@app.get("/bare/{num}")
+def readme(num: int) -> Perf:
+    start = time.time()
+    output = f"hello world {num}!"
+    duration = (time.time() - start) * 1000
+    return {"output": output, "runtime": duration}
+
